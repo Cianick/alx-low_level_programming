e4f57457ba686755962efc48ab3a1de6afcbe6fb
cianick <cianickblack@gmail.com>
2023-06-19 02:46:15 -0700

    after all task are completes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a sentence to the console.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
