3891ab595593fc554dc6e86096841b0f02cd06aa
cianick <cianickblack@gmail.com>
2023-06-20 05:20:08 -0700

    task 5 and 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,13 @@
  */
 int main(void)
 {
-    int number;
+	int digit;
 
-    for (number = 0; number <= 9; number++)
-    {
-        putchar(number + '0');
-        putchar(' ');
-    }
+	for (digit = 0; digit < 10; digit++)
+		putchar(digit + '0');
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
