ff56841cdf7a8d83e06d08676d2b8c69e531e67d
cianick <cianickblack@gmail.com>
2023-06-26 06:01:42 -0700

    the corrections for 2 to 6

--- 0x02-functions_nested_loops/6-abs.c
+++
@@ -1,1 +0,0 @@
-#include "main.h"\n\n/**\n * _abs - computes the absolute value of an integer\n * @n: the int to check\n * Return: the absolute value of int\n */\nint _abs(int n)\n{\n\tif (n >= 0)\n\t{\n\t\treturn (n);\n\t}\n\treturn (-n);\n}
